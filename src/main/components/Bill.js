4422e1237640e11804aabb68e9dbd099172e64d1
reveland <reveland92@gmail.com>
2017-05-01 19:01:15 +0200

    colored bills

--- src/main/components/Bill.js
+++ src/main/components/Bill.js
@@ -7,21 +7,31 @@ class Bill extends Component {
     // let amount = this.props.amount
     // let start = this.props.start
     // let end = this.props.end
+    let typeId = this.props.typeId
     let height = this.props.height
     let width = this.props.width
     let x = this.props.x
-    return this.createBill(height, width, x)
+    return this.createBill(typeId, height, width, x)
   }
 
-  createBill(height, width, x) {
+  createBill(typeId, height, width, x) {
     return <g
       className="Bill"
       transform={"translate(" + x + ", 0)"}>
       <rect
         height={height}
         width={width}
+        fill={this.getColor(typeId)}
       />
     </g>
+  }
+
+  getColor(id) {
+    id = isNaN(id)
+      ? 0
+      : id;
+    const colors = ["#a6cee3", "#1f78b4", "#b2df8a", "#33a02c", "#fb9a99", "#e31a1c", "#fdbf6f", "#ff7f00", "#cab2d6", "#6a3d9a", "#ffff99", "#b15928", "#8dd3c7", "#ffffb3", "#bebada", "#fb8072", "#80b1d3", "#fdb462", "#b3de69", "#fccde5", "#d9d9d9", "#bc80bd", "#ccebc5", "#ffed6f", "darkgreen", "limegreen", "darkblue", "lightskyblue", "darkviolet", "violet", "gold", "orangered", "goldenrod", "blue", "olive", "goldenrod", "yellow", "darkblue", "brown", "blueviolet", "chartreuse", "chocolate", "coral", "deepskyblue", "crimson", "cyan", "darkblue", "darkcyan", "darkgoldenrod", "darkgreen", "darkmagenta", "darkolivegreen", "darkorange", "darkorchid", "darkred", "darksalmon", "darkseagreen", "darkslateblue", "darkslategray", "darkslategrey", "darkturquoise", "darkviolet", "deeppink", "deepskyblue", "dimgray", "dimgrey", "dodgerblue", "firebrick", "floralwhite", "forestgreen", "fuchsia", "gainsboro", "ghostwhite", "gold", "goldenrod", "gray", "green", "greenyellow", "grey", "honeydew", "hotpink", "indianred", "indigo", "ivory", "khaki", "lavender", "lavenderblush", "lawngreen", "lemonchiffon", "lightblue", "lightcoral", "lightcyan", "lightgoldenrodyellow", "lightgray", "lightgreen", "lightgrey", "lightpink", "lightsalmon", "lightseagreen", "lightskyblue", "lightslategray", "lightslategrey", "lightsteelblue", "lightyellow", "lime", "limegreen", "linen", "magenta", "maroon", "mediumaquamarine", "mediumblue", "mediumorchid", "mediumpurple", "mediumseagreen", "mediumslateblue", "mediumspringgreen", "mediumturquoise", "mediumvioletred", "midnightblue", "mintcream", "mistyrose", "moccasin", "navajowhite", "navy", "oldlace", "olive", "olivedrab", "orange", "orangered", "orchid", "palegoldenrod", "palegreen", "paleturquoise", "palevioletred", "papayawhip", "peachpuff", "peru", "pink", "plum", "powderblue", "purple", "red", "rosybrown", "royalblue", "saddlebrown", "salmon", "sandybrown", "seagreen", "seashell", "sienna", "silver", "skyblue", "slateblue", "slategray", "slategrey", "snow", "springgreen", "steelblue", "tan", "teal", "thistle", "tomato", "turquoise", "violet", "wheat", "white", "whitesmoke", "yellowgreen"];
+    return colors[id]
   }
 }
 
--- src/main/components/Type.js
+++ src/main/components/Type.js
@@ -3,7 +3,7 @@ import Bill from './Bill'
 
 class Type extends Component {
     render() {
-        // let id = this.props.id
+        let id = this.props.id
         let bills = this.props.bills
         let name = this.props.name
         let maxAmountPerDay = this.props.maxAmountPerDay
@@ -13,10 +13,10 @@ class Type extends Component {
         let width = this.props.width
         let x = this.props.x
         let y = this.props.y
-        return this.createType(bills, name, maxAmountPerDay, height, width, x, y, start, end)
+        return this.createType(bills, name, maxAmountPerDay, height, width, x, y, start, end, id)
     }
 
-    createType(bills, name, maxAmountPerDay, height, width, x, y, start, end) {
+    createType(bills, name, maxAmountPerDay, height, width, x, y, start, end, id) {
         let billX = 0
         return <g
             className="Type"
@@ -24,14 +24,14 @@ class Type extends Component {
             {bills.map((bill, i) => {
                 let billWidth = this.calculateBillWidth(start, end, bill.start, bill.end, width)
                 let billHeight = this.calculateBillHeight(maxAmountPerDay, bill.amountPerDay, height)
-                bill = this.createBill(bill, name, billHeight, billWidth, billX, i)
+                bill = this.createBill(bill, name, billHeight, billWidth, billX, i, id)
                 billX += billWidth
                 return bill
             })}
         </g>
     }
 
-    createBill(bill, type, height, width, x, i) {
+    createBill(bill, type, height, width, x, i, typeId) {
         return <Bill
             key={i}
             id={bill.id}
@@ -41,6 +41,7 @@ class Type extends Component {
             height={height}
             width={width}
             x={x}
+            typeId={typeId}
         />
     }
 
